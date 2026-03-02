42ef535778d8cd0f91013ef9d91d19caafae7490
dimaspolohov <dimaspolohov@yandex.ru>
2026-03-02 14:15:40 +0300

    New address add

--- wp-content/themes/wp35-shop/assets/js/rs-map.js
+++ wp-content/themes/wp35-shop/assets/js/rs-map.js
@@ -3,35 +3,52 @@ function map(n) {
 	function init() {
 		var myMap = new ymaps.Map("map", {
 			controls: [],
-			center: [59.944229, 30.322617], // Центрируем примерно между точками
+			center: [59.944229, 30.322617],
 			zoom: 13
 		});
 
-		// Первая точка (твоя основная)
-		let myPlacemark1 = new ymaps.Placemark([59.926917, 30.322906], {
-			balloonContent: 'Главный офис'
-		}, {
+		var placemarkOptions = {
 			iconLayout: 'default#imageWithContent',
 			iconImageHref: '/img/logo_black.svg',
 			iconImageSize: [40, 42],
 			iconImageOffset: [-20, -21],
+		};
+
+		var myPlacemark1 = new ymaps.Placemark([59.926917, 30.322906], {
+			balloonContent: 'Гороховая, 49'
+		}, placemarkOptions);
+
+		var myPlacemark2 = new ymaps.Placemark([59.963964, 30.313003], {
+			balloonContent: 'Каменоостровский, 32'
+		}, placemarkOptions);
+
+		var myPlacemark3 = new ymaps.Placemark([55.805786, 37.584649], {
+			balloonContent: 'Большая Новодмитровская, 36 ст. 8'
+		}, placemarkOptions);
+
+		// Cluster layout using the same logo icon, offset mirrors iconImageOffset [-20, -21]
+		var ClusterLayout = ymaps.templateLayoutFactory.createClass(
+			'<div style="position:relative;width:40px;height:42px;margin-left:-20px;margin-top:-25px;">' +
+				'<img src="/img/logo_black.svg" style="width:40px;height:42px;" />' +
+			'</div>'
+		);
+
+		var spbClusterer = new ymaps.Clusterer({
+			clusterIconLayout: ClusterLayout,
+			clusterIconShape: {
+				type: 'Rectangle',
+				coordinates: [[-20, -21], [20, 21]]
+			},
+			groupByCoordinates: false,
+			clusterDisableClickZoom: false,
+			gridSize: 30
 		});
 
-		// Вторая точка — Большая Пушкарская, 47
-		let myPlacemark2 = new ymaps.Placemark([59.963964, 30.313003], {
-			balloonContent: 'Большая Пушкарская улица, 47'
-		}, {
-			iconLayout: 'default#imageWithContent',
-			iconImageHref: '/img/logo_black.svg',
-			iconImageSize: [40, 42],
-			iconImageOffset: [-20, -21],
-		});
+		spbClusterer.add([myPlacemark1, myPlacemark2]);
 
-		// Добавляем обе точки на карту
-		myMap.geoObjects.add(myPlacemark1);
-		myMap.geoObjects.add(myPlacemark2);
+		myMap.geoObjects.add(spbClusterer);
+		myMap.geoObjects.add(myPlacemark3);
 
-		// Автоматически подгоняем карту, чтобы обе точки были видны
 		myMap.setBounds(myMap.geoObjects.getBounds(), {
 			checkZoomRange: true,
 			zoomMargin: 40
--- wp-content/themes/wp35-shop/template-store.php
+++ wp-content/themes/wp35-shop/template-store.php
@@ -10,49 +10,54 @@ $frontpage_id = get_option('page_on_front');
 get_header(); ?>
     <!-- page -->
     <main class="page _page-container">
-        <?
+        <?php
         //if(get_field('slaider')):
         //      rs_slider_store();
         // endif; слайдер
         ?> 
         <!-- rs-store -->
-        <? echo get_field('map', get_the_ID(), false, false); ?>
+        <?php
+        echo get_field('map', get_the_ID(), false, false); ?>
         <section class="rs-store">
             <div class="rs-store_container">
                 <div class="rs-store__contact">
-                    <h3 class="xxl-medium-title"><?=get_field('city') ?></h3>
-                    <div class="rs-store__contact_list">
-                        <div class="rs-store__contact_item">
-                            <h5 class="sm-semibold-title">Каменоостровский, 32</h5>
-                                                        <h5 class="sm-semibold-title"><? the_field('address') ?></h5>
-
-                            <? $data = get_field('online_shop'); ?>
-                            <ul>
-                                <li><a href="tel:<?=preg_replace('~\D+~','',$data['phone'])?>"><?=$data['phone']?></a></li>
-                                <li><span><?=$data['worktime']?></span></li>
-                            </ul>
-                            <p class="section-text"><?_e('Opening hours on holidays:','storefront')?> <br>
-                                <?=$data['worktime_out_shop']?></p>
+                    <div class="rs-store__contact_flex_first">
+                        <h3 class="xxl-medium-title"><?=get_field('city') ?></h3>
+                        <div class="rs-store__contact_list">
+                            <div class="rs-store__contact_item">
+                                <h5 class="sm-semibold-title">Каменоостровский, 32</h5>
+                                <h5 class="sm-semibold-title"><?php
+                                    the_field('address') ?></h5>
+                            </div>
                         </div>
-                        <!-- <div class="rs-store__contact_item">
-                            <h5 class="sm-semibold-title"><? the_field('address') ?></h5>
-                            <? $data = get_field('shop'); ?>
-                            <ul>
-                                <li><a href="tel:<?=preg_replace('~\D+~','',$data['phone'])?>"><?=$data['phone']?></a></li>
-                                <li><span><?=$data['worktime']?></span></li>
-                            </ul>
-                            <p class="section-text"><?_e('Opening hours on holidays:','storefront')?> <br>
-                                <?=$data['worktime_out']?></p>
-                        </div> -->
+                        <h3 class="xxl-medium-title">Москва</h3>
+                        <div class="rs-store__contact_list">
+                            <div class="rs-store__contact_item">
+                                <h5 class="sm-semibold-title">Большая Новодмитровская, 36 ст. 8</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="rs-store__contact_flex_second">
+                        <div class="rs-store__contact_list">
+                            <div class="rs-store__contact_item">
+                                <?php $data = get_field('online_shop'); ?>
+                                <ul>
+                                    <li><span class="sm-semibold-title"><?=$data['worktime']?></span></li>
+                                    <li><a class="sm-semibold-title" href="tel:<?=preg_replace('~\D+~','',$data['phone'])?>"><?=$data['phone']?></a></li>
+                                </ul>
+                                <p class="section-text"><?php _e('Opening hours on holidays:','storefront')?> <br>
+                                    <?=$data['worktime_out_shop']?></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="rs-store__map">
                     <div class="map" id="map"></div>
                 </div>
                 <div class="rs-store__about">
-                    <h3 class="xxl-medium-title"><?
+                    <h3 class="xxl-medium-title"><?php
                     //the_title()?></h3>
-                    <p class="section-text"><?
+                    <p class="section-text"><?php
                     //the_content()?></p>
                 </div>
             </div>
@@ -130,53 +135,54 @@ get_header(); ?>
                                         <div class="form-block">
                                             <div class="form-fields">
                                                 <div class="form-field">
-                                                    <input autocomplete="off" type="text" name="user-name" placeholder="<?_e('Your name','storefront')?>"
+                                                    <input autocomplete="off" type="text" name="user-name" placeholder="<?php _e('Your name','storefront')?>"
                                                            class="rs-input">
                                                 </div>
                                                 <div class="form-field">
-                                                    <input autocomplete="off" type="text" name="user-email" placeholder="<?_e('E-mail','storefront')?>"
+                                                    <input autocomplete="off" type="text" name="user-email" placeholder="<?php _e('E-mail','storefront')?>"
                                                            class="rs-input">
                                                 </div>
                                             </div>
                                             <div class="form-field form-field_select">
-                                                <?
+                                                <?php
                                                 $category_form = get_field('category_form');
                                                 if($category_form && $category_form!='') {
                                                     $categories = preg_split('/\r\n|[\r\n]/', $category_form);
                                                     if($categories) {
                                                         ?>
                                                         <select name="user-category" class="rs-input">
-                                                            <option disabled selected><?_e('Category','storefront')?></option>
-                                                            <?
+                                                            <option disabled selected><?php _e('Category','storefront')?></option>
+                                                            <?php
                                                             foreach($categories as $cat) {
                                                                 ?><option><?=$cat?></option>
-                                                            <? } ?>
+                                                            <?php
+                                                            } ?>
                                                         </select>
-                                                        <?
+                                                        <?php
                                                     }
                                                 } ?>
                                             </div>
                                             <div class="form-field">
                                                 <textarea autocomplete="off" name="user-message"
-                                                          placeholder="<?_e('Ask a question or write your wishes and suggestions','storefront')?>"
+                                                          placeholder="<?php _e('Ask a question or write your wishes and suggestions','storefront')?>"
                                                           class="rs-input"></textarea>
                                             </div>
                                             <div class="form-field">
                                                 <label class="input-file rs-input">
                                                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 119 330.6" style="enable-background:new 0 0 119 330.6;" xml:space="preserve"><path d="M77.7,318c-0.4,0.4-0.8,0.8-1.2,1.2c-7.8,7.2-18.1,11.1-29,11.1c-10.9,0-21.3-3.9-29-11.1c-7.9-7.3-12.3-17-12.3-27.4l0-240c0-13.3,5.6-25.7,15.7-35.1c10-9.2,23.3-14.3,37.4-14.3c14.1,0,27.3,5.1,37.4,14.3c10.1,9.3,15.7,21.8,15.7,35.1l0,180.9c0,3.4-2.7,6.1-6.1,6.1c-3.4,0-6.1-2.7-6.1-6.1l0-180.9c0-9.8-4.2-19.1-11.8-26.1c-7.7-7.1-18.1-11-29.1-11c-11,0-21.3,3.9-29.1,11c-7.6,7-11.8,16.3-11.8,26.1l0,240c0,14.5,13.1,26.3,29.1,26.3c7.9,0,15.3-2.8,20.8-7.9c5.4-5,8.3-11.5,8.3-18.4l0-195.2c0-8.5-7.8-15.5-17.4-15.5c-4.7,0-9.2,1.7-12.5,4.7c-3.2,2.9-4.9,6.7-4.9,10.8l0,136c0,3.4-2.7,6.1-6.1,6.1c-3.4,0-6.1-2.7-6.1-6.1l0-136c0-7.5,3.1-14.5,8.9-19.8c5.6-5.1,12.9-7.9,20.7-7.9c16.3,0,29.6,12.4,29.6,27.7l0,195.2C88.8,301.6,84.9,310.9,77.7,318z"/></svg>
                                                     <input type="file" name="user-attachment" multiple>
-                                                    <span><?_e('Attach file','storefront')?></span>
-                                                    <span class="default hidden"><?_e('Attach file','storefront')?></span>
-                                                    <span class="multi hidden"> <?_e('files','storefront')?></span>
+                                                    <span><?php _e('Attach file','storefront')?></span>
+                                                    <span class="default hidden"><?php _e('Attach file','storefront')?></span>
+                                                    <span class="multi hidden"> <?php _e('files','storefront')?></span>
                                                 </label>
                                                 <input type="hidden" value="">
                                             </div>
                                             <div class="form-messages">
                                                 <div class="form-message success" style="display:none;">
-                                                    <span><?_e('Thank you! Your message has been sent','storefront')?></span>
+                                                    <span><?php _e('Thank you! Your message has been sent','storefront')?></span>
                                                 </div>
                                                 <div class="form-message error" style="display:none;">
-                                                    <span><?_e('One or more fields are filled out incorrectly. Please check the details!','storefront')?></span>
+                                                    <span><?php _e('One or more fields are filled out incorrectly. Please check the details!','storefront')?></span>
                                                 </div>
                                             </div>
                                             <div class="form-footer">
@@ -184,7 +190,7 @@ get_header(); ?>
                                                     <?=get_field('text_form')?>
                                                 </div>
                                                 <div class="form-button">
-                                                    <button type="submit" class="rs-btn _background-btn _black-btn"><?_e('Submit','storefront')?></button>
+                                                    <button type="submit" class="rs-btn _background-btn _black-btn"><?php _e('Submit','storefront')?></button>
                                                 </div>
                                             </div>
                                         </div>
